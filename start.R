7d96195105706952746cf67b30c6aacf9db94883
Anjali Bhatt <ambhatt@stanford.edu>
2017-06-22 14:20:18 -0400

    added wd

--- start.R
+++ start.R
@@ -3,5 +3,6 @@
 # a new edit
 
 # Clean data
-df = read.csv("~/Downloads/MTurk_June 22, 2017_12.01.csv",header = TRUE)
-df_new = df[df$Q17_12_TEXT =="I will answer honestly",]
+setwd("~/sicss_survey")
+df <- read.csv("MTurk_June 22, 2017_12.01.csv",header = TRUE)
+df_new <- df[df$Q17_12_TEXT =="I will answer honestly",]
