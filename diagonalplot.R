2f556d9ebea2f2ca746a2ec7acfbe517363cdf4c
myeomans <mk.yeomans@gmail.com>
2017-06-22 15:56:08 -0400

    plot function

--- diagonalplot.R
+++ diagonalplot.R
@@ -1,15 +1,25 @@
-unweight.match<-merge(truth.match[,c("var","response","pew.estimates")],
-                      guess.match[,c("var","response","estimate")],
-                      by=c("var","response"))
 
-unweight.match$pew.estimates<-as.numeric(unweight.match$pew.estimates)
-unweight.match$estimates<-as.numeric(unweight.match$estimate)
-
-plot(x=unweight.match$pew.estimates,
-     y=unweight.match$estimates,
-     xlim=c(0,100),ylim=c(0,100))
-abline(a=0,b=1,col="forestgreen",lty=3)
-
-
-cor(unweight.match$pew.estimates,
-    unweight.match$estimate)
+match.plot<-function(data1,data2){
+  guess.match<-data1[((data1$var%in%data2$var)&(data1$response%in%data2$response)),]
+  truth.match<-data2[((data2$var%in%data1$var)&(data2$response%in%data1$response)),]
+  
+  match.set<-merge(truth.match[,c("var","response","pew.estimates")],
+                        guess.match[,c("var","response","estimate")],
+                        by=c("var","response"))
+  
+  match.set$pew.estimates<-as.numeric(match.set$pew.estimates)
+  match.set$estimates<-as.numeric(match.set$estimate)
+  
+  plot(x=match.set$pew.estimates,
+       y=match.set$estimates,
+       xlim=c(0,100),ylim=c(0,100),
+       xlab="Pew Estimate",
+       ylab="mTurk Estimate")
+  
+  abline(a=0,b=1,col="forestgreen",lty=3)
+  
+  fit<-cor(unweight.match$pew.estimates,unweight.match$estimate)^2
+  
+  text(x=10,y=95,paste("R^2 =",round(fit,3)))
+}
+match.plot(guesses,truth)
--- labelfix.R
+++ labelfix.R
@@ -1,20 +1,20 @@
 
 truth<-read.csv("pewestimates.csv", stringsAsFactors = F)
-
-
-q.name<-sapply(row.names(unweighted),function(x) strsplit(x,"_")[[1]][1])
-a.name<-sapply(row.names(unweighted),function(x) strsplit(x,"_")[[1]][2])
-
-guesses<-data.frame(var=q.name,
-                    response=a.name,
-                    estimate=unweighted[,1])
-
-guesses$response<-tolower(gsub("."," ",guesses$response,fixed=T))
 truth$response<-tolower(truth$response)
+
+label.fix<-function(data){
+  q.name<-sapply(row.names(data),function(x) strsplit(x,"_")[[1]][1])
+  a.name<-sapply(row.names(data),function(x) strsplit(x,"_")[[1]][2])
+  
+  guesses<-data.frame(var=q.name,
+                      response=a.name,
+                      estimate=data[,1])
+  guesses$response<-tolower(gsub("."," ",guesses$response,fixed=T))
+  return(guesses)
+}
+
+raw.guesses<-label.fix(guesses)
 
 
 table(guesses$var%in%truth$var,
       guesses$response%in%truth$response)
-
-guess.match<-guesses[((guesses$var%in%truth$var)&(guesses$response%in%truth$response)),]
-truth.match<-truth[((truth$var%in%guesses$var)&(truth$response%in%guesses$response)),]
