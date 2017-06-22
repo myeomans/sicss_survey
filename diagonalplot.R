0b1943d2a7fbfa96f64e248d6601cacb872b67b6
myeomans <mk.yeomans@gmail.com>
2017-06-22 15:38:55 -0400

    label fix

--- diagonalplot.R
+++ diagonalplot.R
@@ -5,9 +5,14 @@ truth<-read.csv("pewestimates.csv", stringsAsFactors = F)
 q.name<-sapply(row.names(unweighted),function(x) strsplit(x,"_")[[1]][1])
 a.name<-sapply(row.names(unweighted),function(x) strsplit(x,"_")[[1]][2])
 
-data.frame(var=qname,
-           response=q.name,
-           estimate=unweighted[,1])
+guesses<-data.frame(var=q.name,
+                    response=a.name,
+                    estimate=unweighted[,1])
+
+guesses$response<-tolower(gsub("."," ",guesses$response,fixed=T))
+truth$response<-tolower(truth$response)
+
+
 
 predictions<-sample(1:100, nrow(truth))
 
