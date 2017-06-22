49f8cc5aa60fd0f8a05bc3c7c8bed9c0703f1080
myeomans <mk.yeomans@gmail.com>
2017-06-22 15:33:36 -0400

    labels

--- diagonalplot.R
+++ diagonalplot.R
@@ -1,6 +1,14 @@
 
 truth<-read.csv("pewestimates.csv", stringsAsFactors = F)
 
+
+q.name<-sapply(row.names(unweighted),function(x) strsplit(x,"_")[[1]][1])
+a.name<-sapply(row.names(unweighted),function(x) strsplit(x,"_")[[1]][2])
+
+data.frame(var=qname,
+           response=q.name,
+           estimate=unweighted[,1])
+
 predictions<-sample(1:100, nrow(truth))
 
 
