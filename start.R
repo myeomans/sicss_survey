b317647ba08b60b4c008eb83efb523e72aed967d
Anjali Bhatt <ambhatt@stanford.edu>
2017-06-22 14:16:59 -0400

    added cleaning lines

--- start.R
+++ start.R
@@ -1,3 +1,7 @@
 # welcome, everyone!
 
 # a new edit
+
+# Clean data
+df = read.csv("~/Downloads/MTurk_June 22, 2017_12.01.csv",header = TRUE)
+df_new = df[df$Q17_12_TEXT =="I will answer honestly",]
